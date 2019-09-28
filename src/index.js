897666c3e8ac9c4b3990b4c2031f4c2575b349bf
Frostmourne <cozroman@yandex.ru>
2019-09-29 01:14:25 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let result = true;
+    const configObj = new Map(bracketsConfig);
+    const strArr = str.split('');
+    const bracketsStack = [];
+    for (let i = 0; i < strArr.length; i++) {
+        if (configObj.has(strArr[i]) && configObj.get(bracketsStack[bracketsStack.length - 1]) !== strArr[i]) bracketsStack.push(strArr[i])
+        else {
+            if (!(bracketsStack.length)) {
+                result = false;
+                break;
+            }
+            const lastBracket = bracketsStack.pop();
+            if (!(configObj.get(lastBracket) === strArr[i])) {
+                result = false;
+                break;
+            }
+        }
+    }
+
+    result = result && !bracketsStack.length;
+
+    return result;
 }
